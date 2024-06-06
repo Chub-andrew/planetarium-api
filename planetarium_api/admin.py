6619436bfad01f32d62f913ca5221c9fab421670
Andrii Chubenko <stalkerock@gmail.com>
2024-06-06 15:32:47 +0300

    add models + admin panel for models

--- planetarium_api/admin.py
+++ planetarium_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlanetariumDome, Reservation, ShowTheme, AstronomyShow, ShowSession, Ticket
+
+admin.site.register(PlanetariumDome)
+admin.site.register(Reservation)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
--- planetarium_api/models.py
+++ planetarium_api/models.py
@@ -1,3 +1,85 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.text import slugify
+
+
+def validate_positive(value):
+    if value <= 0:
+        raise ValidationError('Value must be positive.')
+
+
+def show_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/shows/", filename)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField(validators=[validate_positive])
+    seats_in_row = models.IntegerField(validators=[validate_positive])
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reservations')
+
+    def __str__(self):
+        return f"{self.user} {self.created_at}"
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    theme = models.ForeignKey(ShowTheme, on_delete=models.SET_NULL, null=True, blank=True, related_name='astronomy_shows')
+    image = models.ImageField(null=True, upload_to=show_image_file_path)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE, related_name='show_sessions')
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE, related_name='show_sessions')
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return f"{self.astronomy_show.title} {self.planetarium_dome.name} {self.show_time}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name='tickets')
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='tickets')
+
+    class Meta:
+        unique_together = ('row', 'seat', 'show_session')
+        ordering = ["row", "seat"]
+
+    def __str__(self):
+        return f"{self.row} {self.seat}"
