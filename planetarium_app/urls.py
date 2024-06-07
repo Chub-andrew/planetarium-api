dca1c5b417576cf116646c15c084af1b6f11a501
Andrii Chubenko <stalkerock@gmail.com>
2024-06-07 14:50:39 +0300

    start of work with model user

--- planetarium_app/urls.py
+++ planetarium_app/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/planetarium/", include("planetarium_api.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
     path('', RedirectView.as_view(url='api/planetarium/', permanent=True)), #added this for automative loaded
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
